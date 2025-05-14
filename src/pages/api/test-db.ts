8dbc55f9aec20ae35dea28d24af2e4c89deb3979
BasharST99 <telfahb@gmail.com>
2025-05-14 23:04:36 +0300

    Add: Add test-DB to test DataBase

--- src/pages/api/test-db.ts
+++ src/pages/api/test-db.ts
@@ -1,12 +1,13 @@
-import { prisma } from '@/lib/prisma'
-import type { NextApiRequest, NextApiResponse } from 'next'
-
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
     const products = await prisma.product.findMany()
     res.status(200).json(products)
-  } catch (error: unknown) {
-    const errorMessage = error instanceof Error ? error.message : 'DB connection failed';
-    res.status(500).json({ error: errorMessage });
+  } catch (error: any) {
+    console.error("❌ Prisma Error:", error)
+    res.status(500).json({
+      message: "Failed to fetch product",
+      error: error.message,
+      stack: error.stack,
+    })
   }
 }
